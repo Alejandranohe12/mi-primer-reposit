ae488c242741eb411ec3b41fec2a0ecd07b4ed11
Nohely Alejandra <alejandranohe@gmail.com>
2022-11-17 07:41:46 -0400

    mi primer commit

---
+++ PrimerArchivo/SegundoArchivo.py
@@ -0,0 +1,11 @@
+from tkinter import Y
+
+
+x = 10
+y = 2
+z= x + y
+print(z)
+#Aquí se hace una suma sencilla, le asignamos a nuestra variable (x) cualquier valor numerico. En este caso es "10"
+#y para (y) será "2"
+#con la tercera variable (z), ella será la que almacenará los valores de las variables (x) y (y), y donde se realizará la suma
+#con print() le pedimos que nos muestre el resultado almacenado en (z) en pantalla
---
+++ PrimerArchivo/index.py
@@ -0,0 +1,9 @@
+print("Primer codigo")
+miVariable = "Hola mundo"
+print(miVariable)
+#lo primero en hacer es pensar que tipo de variable queremos que sea, en mi caso será tipo texto y el valor/dato que le asiganre será "Hola mundo"
+#lo segundo ya es decirle a python que nos imprima en patalla nuestro valor 
+
+MiVariable = 10
+print(MiVariable)
+#aquí hacemos lo mismo que en el primer ejemplo, pero en debe ser tipo texto, se le asigna un valor/dato numerico
